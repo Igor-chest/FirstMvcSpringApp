1e7094b591868f3ce4bede05931b3de72fd1c029
igor-chest <shesterin51@gmail.com>
2023-03-19 17:38:50 +0500

    mvc start

--- src/main/java/org/example/controllers/FirstController.java
+++ src/main/java/org/example/controllers/FirstController.java
@@ -1,7 +1,17 @@
 package org.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
+    @GetMapping("/hello") // при переходе по адресу вызывается метод
+    public String helloPage() {
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye") // при переходе по адресу вызывается метод
+    public String goodByePage() {
+        return "first/goodbye";
+    }
 }
