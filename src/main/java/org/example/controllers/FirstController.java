73a4d99d75d6ecf4dcfc839ecbc95a18d64bd0f3
igor-chest <shesterin51@gmail.com>
2023-03-19 18:08:36 +0500

    mvc start

--- src/main/java/org/example/controllers/FirstController.java
+++ src/main/java/org/example/controllers/FirstController.java
@@ -2,8 +2,10 @@ package org.example.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/firster")  // меняет переход в имеющихся здесь GetMapping (теперь нужно в адресной строке вбивать это перед разделом)
 public class FirstController {
     @GetMapping("/hello") // при переходе по адресу вызывается метод
     public String helloPage() {
