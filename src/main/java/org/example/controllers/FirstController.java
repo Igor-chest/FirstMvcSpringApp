d60bccd0c09ed7cc2b05048b1c98a3ba2d5014f5
igor-chest <shesterin51@gmail.com>
2023-03-20 17:47:00 +0500

    mvc start

--- src/main/java/org/example/controllers/FirstController.java
+++ src/main/java/org/example/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +12,12 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")  // меняет переход в имеющихся здесь GetMapping (теперь нужно в адресной строке вбивать это перед разделом)
 public class FirstController {
     @GetMapping("/hello") // при переходе по адресу вызывается метод
-    public String helloPage(@RequestParam("name") String name, // передача параметров клиентом в адресной строке
-                            @RequestParam("surname") String surname) {
+    public String helloPage(@RequestParam(value = "name", required = false) String name, // передача параметров клиентом в адресной строке
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println(name + " " + surname);
+        //System.out.println(name + " " + surname);
+        model.addAttribute("message", name + " " + surname); // модель, которую отправим в представление пользователю
 
         return "first/hello";
     }
